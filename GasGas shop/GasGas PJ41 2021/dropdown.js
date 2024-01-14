194cf3ea609abd2a63194ae3dbade633b299650e
MxSimGraphics <119175064+MxSimGraphics@users.noreply.github.com>
2024-01-14 18:51:46 +0200

    Add files via upload

---
+++ GasGas shop/GasGas PJ41 2021/dropdown.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+
+  function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
+  
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn2')) {
+    var myDropdown2 = document.getElementById("myDropdown2");
+      if (myDropdown2.classList.contains('show')) {
+        myDropdown2.classList.remove('show');
+      }
+    }
+  }
