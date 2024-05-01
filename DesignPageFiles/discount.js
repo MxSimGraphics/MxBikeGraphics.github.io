9f59a3f89db5124a3e01d4c6d8ddd714effd65ca
MxSimGraphics <119175064+MxSimGraphics@users.noreply.github.com>
2024-05-01 15:43:34 +0300

    Update discount.js

--- DesignPageFiles/discount.js
+++ DesignPageFiles/discount.js
@@ -11,7 +11,7 @@ function applyDiscount() {
     var originalPrice = selectedSeatCover.includes("Seat cover") ? 17.00 : 15.00;
 
     // Check if discount code is valid
-    if (discountCode === "SummerSale") {
+    if (discountCode === "") {
         var discountedPrice = originalPrice * 0.7; // Calculate 30% discount
         originalPriceElement.style.textDecoration = "line-through"; // Add strikethrough
         originalPriceElement.style.color = "red"; // Change color to red
