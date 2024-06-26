26d53fcd53e4344a0a8443af7701d94fb37979f0
MxSimGraphics <119175064+MxSimGraphics@users.noreply.github.com>
2024-06-26 21:48:53 +0300

    Update discount.js

--- DesignPageFiles/discount.js
+++ DesignPageFiles/discount.js
@@ -1,7 +1,9 @@
+
 function applyDiscount() {
     var discountCode = document.getElementById("discountCode").value;
     var discountCodeUsedInput = document.getElementById("discountCodeUsed");
     var originalPriceElement = document.getElementById("originalPrice");
+    var newPriceElement = document.getElementById("newPrice");
     var selectedSeatCover = document.getElementById("slct5").value; // Get selected seat cover
     
     // Set the value of the hidden input field
@@ -9,11 +11,16 @@ function applyDiscount() {
 
     // Calculate original price based on selected seat cover
     var originalPrice = selectedSeatCover.includes("Seat cover") ? 17.00 : 15.00;
+    var newPrice = selectedSeatCover.includes("Seat cover") ? 11.00 : 9.00;
+
+    // Update the prices on the page
+    originalPriceElement.innerHTML = "$" + originalPrice.toFixed(2);
+    newPriceElement.innerHTML = "$" + newPrice.toFixed(2);
 
     // Define discount codes and their respective discounts
     var discounts = {
-        "gdfgdfgdg": 0.4,    // 40% discount
-        "gdfgdfgdgd": 0.3    // 30% discount
+        // "summersale": 0.4,    // 40% discount
+        // "sponsorship": 0.3    // 30% discount
     };
 
     // Check if the discount code is valid
@@ -21,20 +28,12 @@ function applyDiscount() {
         var discountPercentage = discounts[discountCode];
         var discountedPrice = originalPrice * (1 - discountPercentage); // Calculate discounted price
 
-        // Apply styling and show discounted price
-        originalPriceElement.style.textDecoration = "line-through"; // Add strikethrough
-        originalPriceElement.style.color = "red"; // Change color to red
-        originalPriceElement.innerHTML = "$" + originalPrice.toFixed(2); // Update to show original price
-
+        // Show discounted price
         var discountedPriceElement = document.getElementById("discountedPrice");
         discountedPriceElement.innerHTML = "$" + discountedPrice.toFixed(2); // Display discounted price
         discountedPriceElement.parentNode.style.display = "block"; // Display discounted price section
     } else {
-        // Reset the price to the original price
-        originalPriceElement.innerHTML = "$" + originalPrice.toFixed(2);
-        originalPriceElement.style.textDecoration = "none"; // Remove strikethrough
-        originalPriceElement.style.color = "black"; // Change color back to black
-        
+        // Hide discounted price section
         var discountedPriceElement = document.getElementById("discountedPrice");
         discountedPriceElement.parentNode.style.display = "none"; // Hide discounted price section
     }
